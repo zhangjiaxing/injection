52993c908e025da259b1d1e4cda06c7acd7961bb
zhiliao <zhang.jiaxing@foxmail.com>
2021-10-31 21:32:50 +0800

    fix

--- ld_preload/lib.c
+++ ld_preload/lib.c
@@ -1,4 +1,5 @@
 #include <stdbool.h>
+#include <stdatomic.h>
 
 #define _GNU_SOURCE
 #include <dlfcn.h>
@@ -7,9 +8,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-
-
-bool init_flag = false;
 
 static void* load_sym(char* symname, void* proxyfunc) {
     void *funcptr = dlsym(RTLD_NEXT, symname);
@@ -27,14 +25,15 @@ static void* load_sym(char* symname, void* proxyfunc) {
 }
 
 #define SETUP_SYM(X) do { true_ ## X = load_sym( # X, X ); } while(0)
+#define ORIGIN_CALL(func, ...) true_ ## func(__VA_ARGS__)
 
 typedef size_t (*fread_t)(void*, size_t, size_t, FILE*);
 fread_t true_fread = NULL;
 
 void do_init(){
-    bool ret = init_flag;
-    if(!ret){
-        init_flag = true;
+    static atomic_flag init_flag = ATOMIC_FLAG_INIT;
+
+    if(! atomic_flag_test_and_set(&init_flag)){
         SETUP_SYM(fread);
     }
 }
@@ -43,11 +42,12 @@ size_t fread(void *ptr, size_t size, size_t nmemb, FILE *stream){
     do_init();
 
     char *p = ptr;
-    size_t nread = true_fread(ptr, size, nmemb, stream);
+    size_t nread = ORIGIN_CALL(fread, ptr, size, nmemb, stream);
     for(size_t i=0; i<nread; i++){
         if(isalpha(p[i])){
             p[i] = toupper(p[i]);
         }
     }
+    return nread;
 }
 
