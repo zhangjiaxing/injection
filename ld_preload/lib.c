eca8ab07b9529eff6f9e3b984f26df93afe8093f
zhiliao <zhang.jiaxing@foxmail.com>
2021-10-31 18:12:40 +0800

    fix lib.c

--- ld_preload/lib.c
+++ ld_preload/lib.c
@@ -1,3 +1,6 @@
+#include <stdatomic.h>
+#include <stdbool.h>
+
 #define _GNU_SOURCE
 #include <dlfcn.h>
 
@@ -7,22 +10,21 @@
 #include <ctype.h>
 
 
-static int init_l = 0;
+atomic_flag init_flag = ATOMIC_FLAG_INIT;
 
 static void* load_sym(char* symname, void* proxyfunc) {
-
-	void *funcptr = dlsym(RTLD_NEXT, symname);
-	if(!funcptr) {
-		fprintf(stderr, "Cannot load symbol '%s' %s\n", symname, dlerror());
-		exit(1);
-	} else {
-		fprintf(stderr, "loaded symbol '%s'" " real addr %p  wrapped addr %p\n", symname, funcptr, proxyfunc);
-	}
-	if(funcptr == proxyfunc) {
-		fprintf(stderr, "circular reference detected, aborting!\n");
-		abort();
-	}
-	return funcptr;
+    void *funcptr = dlsym(RTLD_NEXT, symname);
+    if(!funcptr) {
+        fprintf(stderr, "Cannot load symbol '%s' %s\n", symname, dlerror());
+        exit(1);
+    } else {
+        fprintf(stderr, "loaded symbol '%s'" " real addr %p  wrapped addr %p\n", symname, funcptr, proxyfunc);
+    }
+    if(funcptr == proxyfunc) {
+        fprintf(stderr, "circular reference detected, aborting!\n");
+        abort();
+    }
+    return funcptr;
 }
 
 #define SETUP_SYM(X) do { true_ ## X = load_sym( # X, X ); } while(0)
@@ -31,21 +33,21 @@ typedef size_t (*fread_t)(void*, size_t, size_t, FILE*);
 fread_t true_fread = NULL;
 
 void do_init(){
-	if(init_l == 0){
-		SETUP_SYM(fread);
-		init_l = 1;
-	}
+    bool ret = atomic_flag_test_and_set(&init_flag);
+    if(ret){
+        SETUP_SYM(fread);
+    }
 }
 
 size_t fread(void *ptr, size_t size, size_t nmemb, FILE *stream){
-	do_init();
-	
-	char *p = ptr;
-	size_t nread = true_fread(ptr, size, nmemb, stream);
-	for(size_t i=0; i<nread; i++){
-		if(isalpha(p[i])){
-			p[i] = toupper(p[i]);
-		}
-	}
+    do_init();
+
+    char *p = ptr;
+    size_t nread = true_fread(ptr, size, nmemb, stream);
+    for(size_t i=0; i<nread; i++){
+        if(isalpha(p[i])){
+            p[i] = toupper(p[i]);
+        }
+    }
 }
 
